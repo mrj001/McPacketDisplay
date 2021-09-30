627c5f2208d5d6296abec2bb0bb7be9e96667845
mj <mj@Marks-Mac-mini.local>
2021-09-30 12:36:54 -0600

    Implemented a class to define MineCraft Packets

---
+++ McPacketDisplay/Models/MineCraftPacketDefinition.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace McPacketDisplay.Models
+{
+   public class MineCraftPacketDefinition
+   {
+      /// <summary>
+      /// Specifies the source of a MineCraft Packet.
+      /// </summary>
+      /// <remarks>
+      /// The values in this enum correspond to the values listed in
+      /// DirectionType in packets.xsd.
+      /// </remarks>
+      public enum Source
+      {
+         Server,
+         Client
+      }
+
+      private List<FieldDefinition> _fields;
+
+      /// <summary>
+      /// Constructs a new MineCraftPacketDefinition
+      /// </summary>
+      /// <param name="node">An XmlNode as specified by the PacketType in packets.xsd.</param>
+      public MineCraftPacketDefinition(XmlNode node)
+      {
+         if (node.Name != "packet")
+            throw new ArgumentException($"{nameof(node)} must be a <packet> node.");
+
+         XmlNode nameNode = node.FirstChild!;
+         Name = nameNode.InnerText;
+
+         XmlNode fromNode = nameNode.NextSibling!;
+         switch(fromNode.InnerText)
+         {
+            case "server":
+               From = Source.Server;
+               break;
+
+            case "client":
+               From = Source.Client;
+               break;
+
+            default:
+               throw new InvalidCastException($"{fromNode.InnerText} cannot be converted to a source.");
+         }
+
+         XmlNode idNode = fromNode.NextSibling!;
+         ID = new PacketID(idNode);
+
+         _fields = new List<FieldDefinition>();
+         XmlNode fieldsNode = idNode.NextSibling!;
+         XmlNode fieldNode = fieldsNode.FirstChild!;
+         while (fieldNode is not null)
+         {
+            _fields.Add(new FieldDefinition(fieldNode));
+            fieldNode = fieldNode.NextSibling!;
+         }
+      }
+
+      /// <summary>
+      /// Gets the PacketID for the MineCraft Packet defined by this object.
+      /// </summary>
+      public PacketID ID { get; }
+
+      /// <summary>
+      /// Gets the Source (sender) of the MineCraft Packet defined by this object.
+      /// </summary>
+      public Source From { get; }
+
+      /// <summary>
+      /// Gets the Name of the MineCraft Packet defined by this object.
+      /// </summary>
+      public string Name { get; }
+
+      /// <summary>
+      /// Gets the FieldDefinition at the given index.
+      /// </summary>
+      /// <param name="index">The index of the FieldDefinition to return</param>
+      /// <returns></returns>
+      public FieldDefinition this[int index]
+      {
+         get
+         {
+            return _fields[index];
+         }
+      }
+
+
+      // TODO add method to parse the defined MineCraft Packet from a BinaryReader
+
+   }
+}
---
+++ Test/Models/TestMineCraftPacketDefinition.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Xml;
+using McPacketDisplay.Models;
+using Xunit;
+
+namespace Test.Models
+{
+   public class TestMineCraftPacketDefinition
+   {
+      public static TheoryData<int, MineCraftPacketDefinition.Source, string, string[],FieldDefinition.FieldDataType[], string> ctor_TestData
+      {
+         get
+         {
+            var rv = new TheoryData<int, MineCraftPacketDefinition.Source, string, string[], FieldDefinition.FieldDataType[], string>();
+
+            rv.Add(10, MineCraftPacketDefinition.Source.Client, "PlayerGroundedPacket",
+               new string[] { "OnGround" },
+               new FieldDefinition.FieldDataType[] { FieldDefinition.FieldDataType.Bool },
+               "<packet><name>PlayerGroundedPacket</name><from>client</from><id>0a</id><fields><field><name>OnGround</name><type>bool</type></field></fields></packet>");
+
+            rv.Add(0, MineCraftPacketDefinition.Source.Client, "KeepAlivePacket",
+               Array.Empty<string>(), Array.Empty<FieldDefinition.FieldDataType>(),
+               "<packet><name>KeepAlivePacket</name><from>client</from><id>00</id><fields></fields></packet>");
+
+            rv.Add(0x6a,
+               MineCraftPacketDefinition.Source.Server, "TransactionStatusPacket",
+               new string[] { "WindowID", "ActionNumber", "Accepted" },
+               new FieldDefinition.FieldDataType[] { FieldDefinition.FieldDataType.Byte, FieldDefinition.FieldDataType.Short, FieldDefinition.FieldDataType.Bool },
+               "<packet><name>TransactionStatusPacket</name><from>server</from><id>6a</id><fields><field><name>WindowID</name><type>byte</type></field><field><name>ActionNumber</name><type>short</type></field><field><name>Accepted</name><type>bool</type></field></fields></packet>");
+
+            return rv;
+         }
+      }
+
+      [Theory]
+      [MemberData(nameof(ctor_TestData))]
+      public void ctor(int expectedId, MineCraftPacketDefinition.Source expectedSource,
+         string expectedName, string[] expectedFieldNames,
+         FieldDefinition.FieldDataType[] expectedFieldDataTypes,
+         string xml)
+      {
+         XmlDocument doc = new XmlDocument();
+         doc.LoadXml($"<id>{expectedId:x2}</id>");
+         PacketID expectedPacketID = new PacketID(doc.FirstChild);
+
+         doc = new XmlDocument();
+         doc.LoadXml(xml);
+         MineCraftPacketDefinition actual = new MineCraftPacketDefinition(doc.FirstChild);
+
+         Assert.Equal(expectedPacketID, actual.ID);
+         Assert.Equal(expectedSource, actual.From);
+         Assert.Equal(expectedName, actual.Name);
+
+         if (expectedFieldNames.Length != expectedFieldDataTypes.Length)
+            throw new ArgumentException($"{expectedFieldNames} and {expectedFieldDataTypes} must have the same Length.");
+
+         int jul = expectedFieldNames.Length;
+         for (int j = 0; j < jul; j ++)
+         {
+            Assert.Equal(expectedFieldNames[j], actual[j].Name);
+            Assert.Equal(expectedFieldDataTypes[j], actual[j].FieldType);
+         }
+
+         Assert.Throws<ArgumentOutOfRangeException>(() => { FieldDefinition fd = actual[-1]; });
+         Assert.Throws<ArgumentOutOfRangeException>(() => { FieldDefinition fd = actual[jul]; });
+      }
+
+      [Fact]
+      public void ctor_Throws_ArgumentException()
+      {
+         XmlDocument doc = new XmlDocument();
+         doc.LoadXml("<wrong><node>This doesn't belong here</node></wrong>");
+
+         Assert.Throws<ArgumentException>(() => new MineCraftPacketDefinition(doc.FirstChild));
+      }
+
+      [Fact]
+      public void ctor_Throws_InvalidCastException()
+      {
+         XmlDocument doc = new XmlDocument();
+         doc.LoadXml("<packet><name>KeepAlivePacket</name><from>InvalidValue</from><id>00</id><fields></fields></packet>");
+
+         Assert.Throws<InvalidCastException>(() => new MineCraftPacketDefinition(doc.FirstChild));
+      }
+   }
+}
