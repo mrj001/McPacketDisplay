239db058b5689bf138357d66f27cfdfe5970ed4a
mj <mj@Marks-Mac-mini.local>
2021-09-30 11:31:22 -0600

    Implemented a FieldDefinition class

---
+++ McPacketDisplay/Models/FieldDefinition.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Xml;
+
+namespace McPacketDisplay.Models
+{
+   public class FieldDefinition
+   {
+      public enum FieldDataType
+      {
+            Byte,
+            ByteArray,
+            Short,
+            ShortArray,
+            Integer,
+            Long,
+            Float,
+            Double,
+            String8,
+            String16,
+            Bool,
+            Metadata
+      }
+
+      public FieldDefinition(XmlNode node)
+      {
+         if (node.Name != "field")
+            throw new ArgumentException($"{nameof(node)} must be a <field> node.");
+
+         XmlNode nameNode = node.FirstChild!;
+         Name = nameNode.InnerText;
+
+         XmlNode typeNode = nameNode.NextSibling!;
+         FieldType = ParseTypeNode(typeNode);
+      }
+
+      private static FieldDataType ParseTypeNode(XmlNode node)
+      {
+         switch(node.InnerText)
+         {
+            case "byte":
+               return FieldDataType.Byte;
+
+            case "byte[]":
+               return FieldDataType.ByteArray;
+
+            case "short":
+               return FieldDataType.Short;
+
+            case "short[]":
+               return FieldDataType.ShortArray;
+
+            case "int":
+               return FieldDataType.Integer;
+
+            case "long":
+               return FieldDataType.Long;
+
+            case "float":
+               return FieldDataType.Float;
+
+            case "double":
+               return FieldDataType.Double;
+
+            case "string8":
+               return FieldDataType.String8;
+
+            case "string16":
+               return FieldDataType.String16;
+
+            case "bool":
+               return FieldDataType.Bool;
+
+            case "metadata":
+               return FieldDataType.Metadata;
+
+            default:
+               throw new InvalidCastException($"Unable to interpret FieldDataType: {node.InnerText}");
+         }
+      }
+
+      public string Name { get; }
+
+      public FieldDataType FieldType { get; }
+
+      // TODO Add code to parse Field values from a byte stream
+   }
+}
---
+++ Test/Models/TestFieldDefinition.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Xml;
+using McPacketDisplay.Models;
+using Xunit;
+
+namespace Test.Models
+{
+   public class TestFieldDefinition
+   {
+      public static TheoryData<string, FieldDefinition.FieldDataType, string> ctor_TestData
+      {
+         get
+         {
+            var rv = new TheoryData<string, FieldDefinition.FieldDataType, string>();
+            rv.Add("TheName", FieldDefinition.FieldDataType.String8, "<field><name>TheName</name><type>string8</type></field>");
+            rv.Add("EntityID", FieldDefinition.FieldDataType.Integer, "<field><name>EntityID</name><type>int</type></field>");
+            return rv;
+         }
+      }
+
+      [Theory]
+      [MemberData(nameof(ctor_TestData))]
+      public void ctor_Test(string expectedName, FieldDefinition.FieldDataType expectedType, string xml)
+      {
+         XmlDocument doc = new XmlDocument();
+         doc.LoadXml(xml);
+
+         FieldDefinition actual = new FieldDefinition(doc.FirstChild);
+
+         Assert.Equal(expectedName, actual.Name);
+         Assert.Equal(expectedType, actual.FieldType);
+      }
+
+      [Fact]
+      public void ctor_Throws()
+      {
+         XmlDocument doc = new XmlDocument();
+         doc.LoadXml("<field><name>FriedEgg</name><type>UnknownType</type></field>");
+
+         Assert.Throws<InvalidCastException>(() => new FieldDefinition(doc.FirstChild));
+      }
+   }
+}
