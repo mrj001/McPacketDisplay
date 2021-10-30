9b5e678b3bd677e0b014403bcd5647e4b2f12849
mj <mj@Marks-Mac-mini.local>
2021-10-30 13:05:02 -0600

    Added a directional indicator to the packet display

---
+++ McPacketDisplay/Views/PacketSourceConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data;
+using Avalonia.Data.Converters;
+using McPacketDisplay.Models.Packets;
+
+namespace McPacketDisplay.Views
+{
+   public class PacketSourceConverter : IValueConverter
+   {
+      public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+      {
+         switch((PacketSource)value)
+         {
+            case PacketSource.Server:
+               return "S->C";
+
+            case PacketSource.Client:
+               return "C->S";
+
+            default:
+               throw new ApplicationException();
+         }
+      }
+
+      public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
---
+++ Test/Views/TestPacketSourceConverter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+using McPacketDisplay.Models.Packets;
+using McPacketDisplay.Views;
+using Xunit;
+
+namespace Test.Views
+{
+   public class TestPacketSourceConverter
+   {
+      public static TheoryData<string, PacketSource> Convert_TestData
+      {
+         get
+         {
+            var rv = new TheoryData<string, PacketSource>();
+
+            rv.Add("S->C", PacketSource.Server);
+            rv.Add("C->S", PacketSource.Client);
+
+            return rv;
+         }
+      }
+
+      [Theory]
+      [MemberData(nameof(Convert_TestData))]
+      public void Convert(string expected, PacketSource source)
+      {
+         PacketSourceConverter conv = new PacketSourceConverter();
+
+         string actual = (string)conv.Convert(source, typeof(string), null, CultureInfo.InvariantCulture);
+
+         Assert.Equal(expected, actual);
+      }
+
+      [Fact]
+      public void Convert_Throws()
+      {
+         PacketSourceConverter conv = new PacketSourceConverter();
+
+         Assert.Throws<ApplicationException>(() => conv.Convert(42, typeof(string), null, CultureInfo.InvariantCulture));
+      }
+
+      [Fact]
+      public void ConvertBack_Throws()
+      {
+         PacketSourceConverter conv = new PacketSourceConverter();
+
+         Assert.Throws<NotImplementedException>(() => conv.ConvertBack("S->C", typeof(PacketSource), null, CultureInfo.InvariantCulture));
+      }
+
+
+   }
+}
